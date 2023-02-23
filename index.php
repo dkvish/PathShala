0a3d7fcb627b5115fca7f12bb8c3549b3969d280
Deepak Vishwakarma <dkvish.in@gmail.com>
2023-02-23 12:11:33 +0530

    baneer changed

--- index.php
+++ index.php
@@ -1,154 +1,158 @@
 <?php include 'header.php'; ?>
 
-  <!-- Carousel -->
+<!-- Carousel -->
 
-  <div id="demo" class="carousel slide" data-ride="carousel">
+<div id="demo" class="carousel slide" data-ride="carousel">
     <ul class="carousel-indicators">
-      <li data-target="#demo" data-slide-to="0" class="active"></li>
-      <li data-target="#demo" data-slide-to="1"></li>
-      <li data-target="#demo" data-slide-to="2"></li>
+        <li data-target="#demo" data-slide-to="0" class="active"></li>
+        <li data-target="#demo" data-slide-to="1"></li>
+        <li data-target="#demo" data-slide-to="2"></li>
     </ul>
     <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img src="./assets/img/carousel-1.jpg" alt="Los Angeles"  width="1100" height="500">
-        <div class="carousel-caption">
-          <h3>Los Angeles</h3>
-          <p>We had such a great time in LA!</p>
-        </div>   
-      </div>
-      <div class="carousel-item">
-        <img src="./assets/img/carousel-2.jpg" alt="Chicago" width="1100" height="500">
-        <div class="carousel-caption">
-          <h3>Chicago</h3>
-          <p>Thank you, Chicago!</p>
-        </div>   
-      </div>
-      <div class="carousel-item">
-        <img src="./assets/img/carousel-3.jpg" alt="New York" width="1100" height="500">
-        <div class="carousel-caption">
-          <h3>New York</h3>
-          <p>We love the Big Apple!</p>
-        </div>   
-      </div>
+        <div class="carousel-item active">
+            <img src="./assets/img/carousel-1.jpg" alt="Los Angeles" width="1100" height="500">
+            <!-- <div class="carousel-caption">
+                <h3>Los Angeles</h3>
+                <p>We had such a great time in LA!</p>
+            </div> -->
+        </div>
+        <div class="carousel-item">
+            <img src="./assets/img/carousel-2.jpg" alt="Chicago" width="1100" height="500">
+            <!-- <div class="carousel-caption">
+                <h3>Chicago</h3>
+                <p>Thank you, Chicago!</p>
+            </div> -->
+        </div>
+        <div class="carousel-item">
+            <img src="./assets/img/carousel-3.jpg" alt="New York" width="1100" height="500">
+            <!-- <div class="carousel-caption">
+                <h3>New York</h3>
+                <p>We love the Big Apple!</p>
+            </div> -->
+        </div>
     </div>
     <a class="carousel-control-prev" href="#demo" data-slide="prev">
-      <span class="carousel-control-prev-icon"></span>
+        <span class="carousel-control-prev-icon"></span>
     </a>
     <a class="carousel-control-next" href="#demo" data-slide="next">
-      <span class="carousel-control-next-icon"></span>
+        <span class="carousel-control-next-icon"></span>
     </a>
-  </div>
-  
+</div>
 
-  <!-- Our Program -->
-  <span style="align:center;color:green; margin-top:10px;"><h2>Our Program</h2>
-  <div class="container-program">
-    <div class="row">
-        <div class="col-12 col-md-6 col-lg-3">
-            <div class="card" style="background:#e26262;">
-                <img src="./assets/img/1yr.jpg">
-                <div class="card-body">
-                    <div class="AgeGroup font-handee" style="font-family:Handlee";>
-                        <div>
-                            <span>Little Settler
-                         </div>
-                        <div>
-                        <span>Play Group
+
+<!-- Our Program -->
+
+    <div><h2 class="underline" style="margin-top:10px;text-align:center"><span>Our Program</span></h2><br></div>
+    <div class="container-program">
+        <div class="row">
+            <div class="col-12 col-md-6 col-lg-3">
+                <div class="card" style="background:#e26262;">
+                    <img src="./assets/img/1yr.jpg">
+                    <div class="card-body">
+                        <div class="AgeGroup font-handee" style="font-family:Handlee" ;>
+                            <div>
+                                <span>Little Settler</span>
+                            </div>
+                            <div>
+                                <span>Play Group</span>
+                            </div>
                         </div>
+                        <p class="card-text">A playgroup is a type of early childhood education program designed for
+                            young children, usually between the ages of 1 and 4. The primary focus of a playgroup is to
+                            provide young children with opportunities to play, socialize, and interact with their peers.
+
+                            Playgroups are often led by trained educators and provide a safe and supportive environment
+                            for children to explore, learn, and have fun. The activities offered ...</p>
+                        <div class="btn-read-more"><a href="#" class="btn">Read More</a></div>
                     </div>
-                    <p class="card-text">A playgroup is a type of early childhood education program designed for young children, usually between the ages of 1 and 4. The primary focus of a playgroup is to provide young children with opportunities to play, socialize, and interact with their peers.
-
-Playgroups are often led by trained educators and provide a safe and supportive environment for children to explore, learn, and have fun. The activities offered at a playgroup can include free play, structured games, arts and crafts, and story time.
-
-Playgroups can provide important benefits for young children, such as helping them develop social skills, emotional intelligence, and early learning skills. They can also provide parents with support and resources to help them raise their children and support their learning and development.
-
-In some countries, playgroups are also known as parent-and-toddler groups, and they may be offered by schools, community centers, or other organizations. They are typically less structured than preschool programs and may be more focused on play and socialization than academic learning.</p>
-                    <div class="btn-read-more"><a href="#" class="btn">Read More</a></div>
                 </div>
             </div>
-        </div>
-        <div class="col-12 col-md-6 col-lg-3">
-            <div class="card" style="background:#fbd15b;">
-                <img src="./assets/img/2yrs.jpg">
-                <div class="card-body">
-                <div class="AgeGroup">
-                        <div>
-                        <span>Little Grown Up 
-                         </div>
-                        <div>
-                        <span>Nursery
+            <div class="col-12 col-md-6 col-lg-3">
+                <div class="card" style="background:#fbd15b;">
+                    <img src="./assets/img/2yrs.jpg">
+                    <div class="card-body">
+                        <div class="AgeGroup">
+                            <div>
+                                <span>Little Grown Up</span>
+                            </div>
+                            <div>
+                                <span>Nursery</span>
+                            </div>
                         </div>
+                        <p class="card-text card-text-blk">A nursery is a type of early childhood education program for
+                            children between the ages of 2 and 4. The primary focus of a nursery program is to provide
+                            young children with opportunities for play, exploration, and learning in a safe and
+                            supportive environment.
+
+                            Nurseries are typically led by trained educators and provide a variety of activities
+                            designed to support children's development, such as free play, structured games, arts and
+                            crafts,...</p>
+                        <div class="btn-read-more"><a href="#" class="btn">Read More</a></div>
                     </div>
-                    <p class="card-text card-text-blk">A nursery is a type of early childhood education program for children between the ages of 2 and 4. The primary focus of a nursery program is to provide young children with opportunities for play, exploration, and learning in a safe and supportive environment.
-
-Nurseries are typically led by trained educators and provide a variety of activities designed to support children's development, such as free play, structured games, arts and crafts, music and movement, and story time. Nursery programs may also include basic academic concepts, such as numbers, letters, and shapes, as a way to prepare children for formal schooling.
-
-Nursery programs can provide important benefits for young children, such as helping them develop social skills, emotional intelligence, and early learning skills. They can also provide parents with support and resources to help them raise their children and support their learning and development.
-
-In some countries, nursery programs may be part of a larger school system and may be followed by other types of early childhood education programs, such as preschool or kindergarten. In other countries, nurseries may be standalone programs offered by private organizations, community centers, or other entities.</p>
-                    <div class="btn-read-more"><a href="#" class="btn">Read More</a></div>
                 </div>
             </div>
-        </div>
 
-        <div class="col-12 col-md-6 col-lg-3">
-            <div class="card" style="background:#28a745;">
-                <img src="./assets/img/3yrs.jpg">
-                <div class="card-body">
-                <div class="AgeGroup">
-                        <div>
-                        <span>Little Junior
-                         </div>
-                        <div>
-                        <span>LKG
+            <div class="col-12 col-md-6 col-lg-3">
+                <div class="card" style="background:#28a745;">
+                    <img src="./assets/img/3yrs.jpg">
+                    <div class="card-body">
+                        <div class="AgeGroup">
+                            <div>
+                                <span>Little Junior</span>
+                            </div>
+                            <div>
+                                <span>LKG</span>
+                            </div>
                         </div>
+                        <p class="card-text">LKG, which stands for Lower Kindergarten, is a term used in India and some
+                            other countries to refer to the first level of formal education for young children,
+                            typically between the ages of 4 and 5. LKG is considered to be the first step in a child's
+                            formal education and prepares them for future academic learning and development.
+
+                            LKG programs typically focus on introducing children to basic academic concepts, such as
+                            numbers, letters...</p>
+                        <div class="btn-read-more"><a href="#" class="btn">Read More</a></div>
                     </div>
-                    <p class="card-text">LKG, which stands for Lower Kindergarten, is a term used in India and some other countries to refer to the first level of formal education for young children, typically between the ages of 4 and 5. LKG is considered to be the first step in a child's formal education and prepares them for future academic learning and development.
-
-LKG programs typically focus on introducing children to basic academic concepts, such as numbers, letters, and shapes, as well as developing social and emotional skills. The curriculum may also include activities such as play, music, art, and movement, which help to support children's overall development and well-being.
-
-In India, LKG is followed by Upper Kindergarten (UKG) and then primary school. Children who complete LKG are usually considered to be well-prepared for the next stage of their education and are expected to have acquired important skills and knowledge that will serve as a foundation for their future learning.
-
-It is important to note that the educational systems and terms used in different countries may vary, and the definition and content of LKG programs can vary from region to region.</p>
-                    <div class="btn-read-more"><a href="#" class="btn">Read More</a></div>
                 </div>
             </div>
-        </div>
 
-        <div class="col-12 col-md-6 col-lg-3">
-            <div class="card" style="background:#17a2b8;">
-                <img src="./assets/img/4yrs.jpg">
-                <div class="card-body">
-                    <div class="AgeGroup">
-                        <div>
-                        <span>Little Senior
+            <div class="col-12 col-md-6 col-lg-3">
+                <div class="card" style="background:#17a2b8;">
+                    <img src="./assets/img/4yrs.jpg">
+                    <div class="card-body">
+                        <div class="AgeGroup">
+                            <div>
+                                <span>Little Senior</span>
+                            </div>
+                            <div>
+                                <span>UKG</span>
+                            </div>
                         </div>
-                        <div>
-                        <span>UKG
-                        </div>
+                        <p class="card-text">UKG, which stands for Upper Kindergarten, is a term used in India and some
+                            other countries to refer to the second level of formal education for young children,
+                            typically between the ages of 5 and 6. UKG is a continuation of the education received in
+                            the lower kindergarten (LKG) and prepares children for future academic learning and
+                            development.
+
+                            UKG programs typically build on the skills and knowledge acquired in LKG...</p>
+                        <div class="btn-read-more"><a href="#" class="btn">Read More</a></div>
                     </div>
-                    <p class="card-text">UKG, which stands for Upper Kindergarten, is a term used in India and some other countries to refer to the second level of formal education for young children, typically between the ages of 5 and 6. UKG is a continuation of the education received in the lower kindergarten (LKG) and prepares children for future academic learning and development.
-
-UKG programs typically build on the skills and knowledge acquired in LKG and focus on further developing basic academic concepts, such as numbers, letters, and shapes, as well as social and emotional skills. The curriculum may also include activities such as play, music, art, and movement, which help to support children's overall development and well-being.
-
-In India, UKG is followed by primary school and is considered to be an important step in a child's formal education. Children who complete UKG are usually considered to be well-prepared for the next stage of their education and are expected to have acquired important skills and knowledge that will serve as a foundation for their future learning.
-
-It is important to note that the educational systems and terms used in different countries may vary, and the definition and content of UKG programs can vary from region to region.</p>
-                    <div class="btn-read-more"><a href="#" class="btn">Read More</a></div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
 
 
 
     <!-- JavaScripts files here -->
 
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+    </script>
     <script src="./assets/js/bootstrap.bundle.js"></script>
-    
-</body>
-<?php include 'footer.php'; ?>
-</html>
+
+    </body>
+    <?php include 'footer.php'; ?>
+
+    </html>
